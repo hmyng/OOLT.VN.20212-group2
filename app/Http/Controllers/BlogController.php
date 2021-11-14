22766637e62404ae0d1c8e42c6060e2c22e44137
TrangNgan248 <trangnganvirgo@gmail.com>
2021-11-14 15:55:59 +0700

    add Blog and User controller

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index(){
+        $blog = Blog::orderBy('id', 'DESC')->get();
+        return view('home', ["blog"=>$blog]);
+    }
+}
