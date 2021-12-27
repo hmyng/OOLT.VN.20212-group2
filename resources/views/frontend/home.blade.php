1436ceedd78bc094a0dfb9a3c1a48478fcd30dda
HoangAnhNguyen0710 <vipboy69798999@gmail.com>
2021-12-27 22:24:29 +0700

    Them trang dang bai va test form lan 1

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -503,12 +503,6 @@
 
     </div><!-- end wrapper -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script>
-        axios.put('/web-api/blog/2', {
-        blog_heading: "123abc",
-        blog_content: "aijkfdnwei",
-        cat_id: 2,
-})
-    </script>
+
 @endsection
 
--- resources/views/frontend/post_a_status.blade.php
+++ resources/views/frontend/post_a_status.blade.php
@@ -16,18 +16,18 @@
                     {{-- <li class="breadcrumb-item"><a href="#"></a></li> --}}
                     <li class="breadcrumb-item active">Đăng_Bài</li>
                 </ol>
-                <form action="/home">
+                <form action="" id="post_a_status">
                     <div class="mb-3 mt-3">
                       <label for="heading">Tiêu đề :</label>
-                      <input type="text" placeholder="Nhập tiêu đề" name="heading" class="form-control mt-2 mb-3">
-                      <label for="topic">Chủ đề :</label>
+                      <input type="text" placeholder="Nhập tiêu đề" name="heading" id="heading" class="form-control mt-2 mb-3">
+                      <label for="category">Chủ đề :</label>
                       <div class="form-floating mb-3 mt-2">
-                        <select class="form-select" id="sel1" name="sellist" style="height: 38px">
-                          <option>Tiếng Nhật</option>
-                          <option>Kiến thức IT</option>
-                          <option>Chia sẻ kinh nghiệm học</option>
-                          <option>Chia sẻ tài liệu</option>
-                          <option>Khác..</option>
+                        <select class="form-select" id="category" name="category" style="height: 38px">
+                          <option value="1">Tiếng Nhật</option>
+                          <option value="2">Kiến thức IT</option>
+                          <option value="3">Chia sẻ kinh nghiệm học</option>
+                          <option value="4">Chia sẻ tài liệu</option>
+                          <option value="5">Khác..</option>
                         </select>
                         {{-- <label for="sel1" class="form-label"></label> --}}
                       </div>
@@ -42,6 +42,21 @@
         </div>
     </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script>
+
+function submit_form(){
+    $('#post_a_status').on('submit', function (event){
+        event.preventDefault();
+        axios.post('/web-api/blog', {
+        blog_heading: $('#heading').val(),
+        blog_content: $('#post-content').val(),
+        cat_id: $('#category').val(),
+        blog_display : "abcxyz",
+})
+    })
+}
+    </script>
 </section>
 
 
