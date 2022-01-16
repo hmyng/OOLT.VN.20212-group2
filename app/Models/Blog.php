1a5afffe021fd866a0f178187cbad2d6d933ba42
TrangNgan248 <trangnganvirgo@gmail.com>
2022-01-16 10:14:36 +0700

    edit model

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -15,4 +15,16 @@ class Blog extends Model
     {
         return $this->belongsTo(User::class, 'author_id');
     }
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'cat_id');
+    }
+    public function edit()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+    public function delete()
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
 }
--- app/Models/Like.php
+++ app/Models/Like.php
@@ -9,4 +9,10 @@ class Like extends Model
 {
     use HasFactory;
     protected $table = 'likes';
+    public function blogs(){
+        $this->belongsTo(Blog::class, 'blog_id');
+    }
+    public function users(){
+        $this->belongsTo(User::class, 'liker_id');
+    }
 }
